7104687ba33113a13cd95c05b802069214215b2d
davideperucchini <37082014+davideperucchini@users.noreply.github.com>
2020-01-10 10:58:54 +0100

    upload assignment

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
-function preload(){
-  // put preload code here
+var lego;
+
+function preload() {
+  lego = loadModel('assets/lego.obj', true);
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL); //add webgl
+  angleMode(DEGREES);
 }
 
+
 function draw() {
-  // put drawing code here
+  background(color('#121212'));
+
+  //rotation
+  rotateX(180);
+  rotateY(frameCount * 1.0);
+  let locX = mouseX - width / 2;
+  let locY = mouseY - height / 2;
+
+  //lights
+  ambientLight(80, 80, 100);
+  pointLight("#ff4fff", locX, locY, 50);
+  pointLight("#4ff6ff", -locX, -locY, 50);
+
+  noStroke();
+  model(lego);
+
 }
